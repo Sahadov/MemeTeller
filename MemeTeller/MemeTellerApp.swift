a3f0f853a5c0068dabb3b35966fc9323f418308e
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-02-05 21:54:45 -0300

    Add skeleton

---
+++ MemeTeller/MemeTellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeTellerApp.swift
+//  MemeTeller
+//
+//  Created by Dmitry Volkov on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemeTellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
